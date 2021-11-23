1d2924de83922e63cb36e8786ef16c8e3febd0df
Abdullah <abdullah_a_s@hotmail.co.uk>
2021-11-23 13:57:14 +0000

    updated psuedocode in the main.js

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
+//import the translateMorse function
 import { translateMorse, object } from "./translator.js"
 
+//select the html elements via queryselectors
 let english = document.querySelector("#english");
 let morse = document.querySelector("#morse");
 let translateBtn = document.querySelector(".translate");
 
+//apply a click event to the translate button so that when the button is clicked
+//it applies the translateMorse function to the english textarea and then displays 
+//the translation in the morse textarea
 const translateClick = translateBtn.addEventListener("click", () => {
     morse.value = translateMorse(english.value);
 });
